0f00747264f9aaa706d6d0197880881a4fe2c502
SvetoslavNikov <svetoslavnikov794@gmail.com>
2025-08-26 12:44:16 +0300

    Intial working api-gateway. More routes to come

--- src/main/java/com/ridetogether/api_gateway/ApiRouter.java
+++ src/main/java/com/ridetogether/api_gateway/ApiRouter.java
@@ -1,17 +1,17 @@
-package com.ridetogether.api_gateway;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.ServerResponse;
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
-
-@Configuration
-public class ApiRouter {
-
-    @Bean
-    public RouterFunction<ServerResponse> router() {
-        return route(GET("/test"), request -> ServerResponse.ok().bodyValue("Hello WebFlux"));
-    }
-}
+//package com.ridetogether.api_gateway;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.reactive.function.server.RouterFunction;
+//import org.springframework.web.reactive.function.server.ServerResponse;
+//import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+//import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+//
+//@Configuration
+//public class ApiRouter {
+//
+//    @Bean
+//    public RouterFunction<ServerResponse> router() {
+//        return route(GET("/test"), request -> ServerResponse.ok().bodyValue("Hello WebFlux"));
+//    }
+//}
